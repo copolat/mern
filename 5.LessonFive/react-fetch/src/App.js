1353dfee07ef516b255159d839bf752a2ac2cba6
copolat <copolat@yahoo.com>
2021-04-20 07:39:31 -0500

    Routing, Link, NavLink examples

--- 5.LessonFive/react-fetch/src/App.js
+++ 5.LessonFive/react-fetch/src/App.js
@@ -1,12 +1,30 @@
-import UserList from './UserList' 
-import './App.css';
+import UserList from "./UserList";
+import About from './About';
+import "./App.css";
+import { BrowserRouter, Route, Link, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>APP Component</h1>
-      <UserList />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>APP Component</h1>
+        <Link to="/">Home</Link><br/>
+        <Link to="/Contact">Contact</Link><br/>
+        <Link to="/AboutPage">About</Link><br/>
+        <Link to="/AboutPage/1">About 1</Link><br/>
+        {/* <NavLink activeStyle={{color:"red"}} to="/AboutPage/22">About 22</NavLink><br/>
+        <NavLink activeStyle={{color:"red"}} to="/AboutPage/33/33">About 333</NavLink> */}
+        <NavLink activeClassName="activeClass" to="/AboutPage/22">About 22</NavLink><br/>
+        <NavLink activeClassName="activeClass" to="/AboutPage/33/33">About 333</NavLink>
+        <Route path="/" exact strict render={()=>{return <h1>Welcome to Home Page </h1>}}/>
+        <Route path="/Contact" exact strict render={()=>{return <h1>Contact Page </h1>}}/>
+        <Route path="/AboutPage" exact strict component={About}/>
+        <Route path="/AboutPage/:id" exact strict component={About}/>
+        <Route path="/AboutPage/:name/:age" exact  component={About}/>
+        <Route path="/Users" exact strict component={UserList}/>
+        <Route path="/User2" exact strict> <UserList/></Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- 5.LessonFive/react-fetch/src/UserList.js
+++ 5.LessonFive/react-fetch/src/UserList.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react'
+import axios from "axios";
+import React, { Component } from "react";
 
 export default class UserList extends Component {
-  state={user:[]}
+  state = { users: [], isLoding: true };
   componentDidMount() {
-         setTimeout(() => {
-          fetch('https://jsonplaceholder.typicode.com/users')
-          .then((response) => response.json())
-          .then((json) => console.log(json));
-      
-     }, 10000);
-
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    // .then((result) => result.json())
+    // // .then((jsonData) => console.log(jsonData));
+    // .then((jsonData) => this.setState({users:jsonData, isLoding:false}));
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((result) =>this.setState({users:result.data, isLoding:false}))
   }
-  
+
   render() {
+    //console.log(this.state.users)
+    const {isLoding, users} = this.state
     return (
       <div>
-        <h2>USERLIST Component</h2>
+        <h2>USERLIST </h2>
+      {isLoding === true ? "Loading..." : this.state.users.map((item)=>{
+        return <div key={item.id}> {item.name}</div>
+      })}
+        {/* {this.state.users.map((item)=>{
+          return <div  key={item.id}>{item.name} {item.username}</div>
+  })} */}
       </div>
-    )
+    );
   }
 }
