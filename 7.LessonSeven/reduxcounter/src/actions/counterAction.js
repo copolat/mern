dfb9707c2f886c95e5f1d33d6438bcf9a151b3bc
copolat <copolat@yahoo.com>
2021-05-11 22:49:11 -0500

    Counter App with redux

---
+++ 7.LessonSeven/reduxcounter/src/actions/counterAction.js
@@ -0,0 +1,8 @@
+import {  INCREASE_COUNT, DECREASE_COUNT } from './actionTypes'
+
+export const increaseCount = ()=>{
+  return {type: INCREASE_COUNT}
+}
+export const decreaseCount = ()=>{
+  return {type: DECREASE_COUNT}
+}
